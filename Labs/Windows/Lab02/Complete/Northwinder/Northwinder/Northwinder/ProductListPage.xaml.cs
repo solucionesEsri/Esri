78f8636fcfc5ea8fba94414c0f6ed4a905d3ba3b
Jason Bell <jason.bell@treeloop.com>
2017-03-01 19:11:10 -0800

    Lab files for Windows

---
+++ Labs/Windows/Lab02/Complete/Northwinder/Northwinder/Northwinder/ProductListPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Northwinder
+{
+    public partial class ProductListPage : ContentPage
+    {
+        private ProductListViewModel _pvm;
+
+        public ProductListPage()
+        {
+            InitializeComponent();
+
+            var r = new Repository();
+            _pvm = new ProductListViewModel(r.GetAllProducts());
+            BindingContext = _pvm;
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var product = e.SelectedItem as Product;
+            if (product != null) {
+                listView.SelectedItem = null;
+                await Navigation.PushAsync(new ProductPage(product));
+            }
+        }
+    }
+}
---
+++ Labs/Windows/Lab03/Begin/Northwinder/Northwinder/Northwinder/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwinder
+{
+    public class Repository
+    {
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Product One", Package = "Box", UnitPrice = 10.00, IsDiscontinued = false },
+                new Product() { Id = 2, Name = "Product Two", Package = "Bottle", UnitPrice = 5.00, IsDiscontinued = false }
+            };
+        }
+    }
+}
